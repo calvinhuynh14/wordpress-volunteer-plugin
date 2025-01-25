8a751f71cc825ebccfed3654fd83ff6fd01577d6
calvinhuynh14 <huynh.calvin@outlook.com>
2025-01-25 00:30:17 -0500

    Added 'Volunteer' menu page to admin menu. Started an admin page for "Volunteer". Added some comments to functions

--- volunteer_plugin.php
+++ volunteer_plugin.php
@@ -11,6 +11,7 @@
 function volunteer_activation() {
     global $wpdb;
 
+    // Generate the SQL query to create the table
     $wpdb->query ("CREATE TABLE wp_Opportunities(
                    OpportunityID INT AUTO_INCREMENT PRIMARY KEY,
                    Position VARCHAR(100) NOT NULL,
@@ -23,21 +24,40 @@ function volunteer_activation() {
                    Skills_required TEXT NOT NULL
                    );
                 ");
-
+    // Insert a sample opportunity
     $wpdb->query("INSERT INTO wp_Opportunities(Position, Organization, Type, Email, Description, Location, Hours, Skills_required)
                   VALUES('Painter', 'Charity Foundation', 'one-time', 'tester@gmail.com', 'Painting a house for my old uncle', '1234 Elm St', 4, 'Painting skills');
                 ");
 }
-
 register_activation_hook(__FILE__, 'volunteer_activation');
 
-
+// Deactivation Hook - Drops database table
 function volunteer_deactivation() {
     global $wpdb;
-
+    // Drop the table
     $wpdb->query("DROP TABLE IF EXIST Opportunities");
 }
-
 register_deactivation_hook(__FILE__, 'volunteer_deactivation');
+
+// Add to the admin menu
+add_action( 'admin_menu', 'volunteer_plugin_menu' );
+function volunteer_plugin_menu() {
+    add_menu_page(
+        'Volunteer Opportunities',      // Page title
+        'Volunteers',                   // Menu title (sidebar)
+        'manage_options',               // Capability (who can access)
+        'volunteer-opportunity-plugin', // Menu slug
+        'volunteer_admin_page',         // Function to call
+    );
+}
+
+function volunteer_admin_page() {
+    ?>
+    <h1>Volunteer Opportunities</h1>
+    <?php
+
+}
+
+
 
 ?>
